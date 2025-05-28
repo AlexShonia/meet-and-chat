3670b0da6a26dddc5ced0d47861bcee0346a7b9c
AlexShonia <alex.shonia123@gmail.com>
2025-05-28 15:27:07 +0400

    add sessions to admin

--- app/meet_n_chat/admin.py
+++ app/meet_n_chat/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from .models import User
+from django.contrib.sessions.models import Session
 
 admin.site.register(User)
 
+@admin.register(Session)
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ['session_key', 'get_decoded', 'expire_date']
+
+    def get_decoded(self, obj):
+        return obj.get_decoded()
